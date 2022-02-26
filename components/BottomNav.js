27a900493379f2ed79d03239a8a13119876a76b5
Ratama Adhi Nugraha <ratama.adhi@gmail.com>
2022-02-27 01:09:55 +0700

    refactor(BottomNav): improve ui

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -9,7 +9,7 @@ import {
 
 function BottomNav() {
   return (
-    <div className="fixed bottom-0 w-full h-20 flex justify-evenly items-end mt-4 pb-2 border-t border-violet-200/50">
+    <div className="fixed bottom-0 w-full h-[84px] flex justify-around items-end pb-2 border-t border-violet-200/50 dark:border-neutral-700/50">
       <div className="flex flex-col justify-center items-center space-y-1">
         <div className="p-3 border border-violet-200 dark:border-neutral-700 rounded-xl dark:shadow-neutral-900/50">
           <HiOutlineHome />
@@ -24,7 +24,7 @@ function BottomNav() {
       </div>
       <div className="flex flex-col justify-center items-center space-y-1">
         <div className="p-3 bg-violet-300 dark:text-violet-900 border border-violet-400 dark:border-neutral-700 rounded-xl dark:shadow-neutral-900/50 shadow-md shadow-violet-300">
-          <HiOutlinePlusSm size={32} />
+          <HiOutlinePlusSm size={36} />
         </div>
         <p className="text-xs">Transaction</p>
       </div>
